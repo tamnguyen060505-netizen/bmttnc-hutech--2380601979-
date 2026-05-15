557e5e83d25b8c482de9df5a5cc343416e03bb30
tamnguyen060505-netizen <tamnguyen060505@gmail.com>
2026-05-15 09:55:18 +0700

    lab01 hello world

---
+++ lab-01/ex02/ex02_02.py
@@ -0,0 +1,6 @@
+#Nhap ban kinh tu nguoi dung
+Ban_kinh = float(input("Nhap ban kinh cua hinh tron: "))
+#Tinh dien tich cua hinh tron
+Dien_tich = 3.14 * (Ban_kinh ** 2)
+#In ra dien tich cua hinh tron ra man hinh
+print("Dien tich cua hinh tron la: ",Dien_tich)
