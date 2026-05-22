090edc32eccb382cefbacacc286ad65ee655f469
tamnguyen060505-netizen <tamnguyen060505@gmail.com>
2026-05-22 08:38:49 +0700

    [add] lab-01 ex03

---
+++ lab-01/ex03/ex03_05.py
@@ -0,0 +1,16 @@
+def dem_so_lan_xuat_hien(lst):
+    count_dict = {}
+    for item in lst:
+        if item in count_dict:
+            count_dict[item] += 1
+        else:
+            count_dict[item] = 1
+    return count_dict
+    
+#Nhap danh sach tu nguoi dung
+input_string = input("Nhap danh sach cac tu, cach nhau bang dau cach: ")
+word_list = input_string.split()
+
+#Su dung ham va in ket qua
+so_lan_xuat_hien = dem_so_lan_xuat_hien(word_list)
+print("So lan xuat hien cua cac phan tu:",so_lan_xuat_hien)
