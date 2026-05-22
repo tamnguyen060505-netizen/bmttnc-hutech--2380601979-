21f1a5c88a489f9bf838599e030a13e3494a4f81
tamnguyen060505-netizen <tamnguyen060505@gmail.com>
2026-05-22 08:46:47 +0700

    [add] lab-01 ex02

--- lab-01/ex02/ex02_07.py
+++ lab-01/ex02/ex02_07.py
@@ -6,7 +6,7 @@ while True:
     if line.lower() == 'done':
         break
     lines.append(line)
-    #Chuyen cac dong thanh chu in hoa va in ra man hinh
-    print("\nCac dong da nhap sau khi chuyen thanh chu in hoa: ")
-    for line in lines:
-        print(line.upper())
+ #Chuyen cac dong thanh chu in hoa va in ra man hinh
+print("\nCac dong da nhap sau khi chuyen thanh chu in hoa: ")
+for line in lines:
+    print(line.upper())
--- lab-01/ex02/ex02_08.py
+++ lab-01/ex02/ex02_08.py
@@ -5,11 +5,11 @@ def chia_het_cho_5(so_nhi_phan):
         return True
     else:
         return False
-    chuoi_so_nhi_phan = input("Nhap chuoi so nhi phan (phan tach boi dau phay): ")
-    so_nhi_phan_list = chuoi_so_nhi_phan.split(',')
-    so_chia_het_cho_5 = [so for so in so_nhi_phan_list if chia_het_cho_5(so)]
-    if len(so_chia_het_cho_5) > 0:
-        ket_qua = ','.join(so_chia_het_cho_5)
-        print("Cac so nhi phan chia het cho 5 la: ", ket_qua)
-    else:
-        print("Khong co so nhi phan nao chia het cho 5 trong chuoi da nhap.")
+chuoi_so_nhi_phan = input("Nhap chuoi so nhi phan (phan tach boi dau phay): ")
+so_nhi_phan_list = chuoi_so_nhi_phan.split(',')
+so_chia_het_cho_5 = [so for so in so_nhi_phan_list if chia_het_cho_5(so)]
+if len(so_chia_het_cho_5) > 0:
+    ket_qua = ','.join(so_chia_het_cho_5)
+    print("Cac so nhi phan chia het cho 5 la: ", ket_qua)
+else:
+    print("Khong co so nhi phan nao chia het cho 5 trong chuoi da nhap.")
